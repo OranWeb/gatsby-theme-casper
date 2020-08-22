d1b202a2a8f74511da2c701b969fe3506ddf493f
OranWeb <46417116+OranWeb@users.noreply.github.com>
2020-08-22 21:04:54 +0530

    Update gatsby-config.js

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -19,20 +19,20 @@ exports.onPreBootstrap = ({ store, reporter }) => {
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
+    title: 'TornHub',
+    description: 'A Torn City Fan Site',
     coverImage: 'img/blog-cover.jpg',
     logo: 'img/ghost-logo.png',
     lang: 'en',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
-    facebook: 'https://www.facebook.com/ghost',
-    twitter: 'https://twitter.com/tryghost',
+    siteUrl: 'https://torn.oran.pw', // full path to blog - no ending slash
+    facebook: '',
+    twitter: '',
     showSubscribe: false, // subscribe button in site nav and home page
     mailchimpAction: '', // 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
     mailchimpName: '', // 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
     mailchimpEmailFieldName: '', // 'MERGE0',
     googleSiteVerification: '', // 'GoogleCode',
-    footer: 'is based on Gatsby Casper',
+    footer: 'is Developed and Maintained by ORAN',
     postsPerPage: 6,
   },
   mapping: {
